c0279cbe1b9c24c870d943ec073abcf9e5f6e399
Brandon Harris <148388555+brandoncharris@users.noreply.github.com>
2023-10-31 16:46:44 -0500

    Add files via upload
    Removed a lot of bloat to focus on this data cloud. I want to have the categories change dynamically based off the number of tagged posts inside them.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-// Sample data: The number of posts or files for each category
-// Ideally, this data would come from your backend or file system
+// Sample data: The number of items for each category
+// Ideally, this data would come from the server or database
 const categoryCounts = {
-  technology: 5, // example count
-  arts: 2, // example count
-  // ... other categories
+    tech: 15, // example count
+    arts: 2, // example count
+    // ... other categories
 };
 
 // Define base size and increment
@@ -13,16 +13,16 @@ const incrementSize = 10; // 10px
 // Calculate sizes for each category
 const categorySizes = {};
 for (let category in categoryCounts) {
-  categorySizes[category] = baseSize + (incrementSize * categoryCounts[category]);
+    categorySizes[category] = baseSize + (incrementSize * categoryCounts[category]);
 }
 
 // When the DOM is loaded, set the sizes for each category
 document.addEventListener("DOMContentLoaded", () => {
-  for (let category in categorySizes) {
-    const element = document.querySelector(`.${category}`);
-    if (element) {
-      element.style.width = `${categorySizes[category]}px`;
-      element.style.height = `${categorySizes[category]}px`;
+    for (let category in categorySizes) {
+        const element = document.querySelector(`#${category}`);
+        if (element) {
+            element.style.width = `${categorySizes[category]}px`;
+            element.style.height = `${categorySizes[category]}px`;
+        }
     }
-  }
 });
