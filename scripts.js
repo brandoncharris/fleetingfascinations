d772930039c8ec55c674b0392207d6482b3bfb5c
Brandon Harris <148388555+brandoncharris@users.noreply.github.com>
2023-10-31 16:57:25 -0500

    Add files via upload
    Applied a combination of trigonometry and CSS transforms to set the position for each category dynamically.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,42 @@
-// Sample data: The number of items for each category
-// Ideally, this data would come from the server or database
-const categoryCounts = {
-    tech: 15, // example count
-    arts: 2, // example count
-    // ... other categories
+// Define category sizes
+const categorySizes = {
+    "sports": 100,
+    "entertainment": 150,
+    "technology": 200,
+    "health": 180,
+    "travel": 160
 };
 
-// Define base size and increment
-const baseSize = 50; // 50px
-const incrementSize = 10; // 10px
-
-// Calculate sizes for each category
-const categorySizes = {};
-for (let category in categoryCounts) {
-    categorySizes[category] = baseSize + (incrementSize * categoryCounts[category]);
-}
-
-// When the DOM is loaded, set the sizes for each category
 document.addEventListener("DOMContentLoaded", () => {
+    // Find the center of the categories-container
+    const container = document.querySelector('.categories-container');
+    const containerCenterX = container.offsetWidth / 2;
+    const containerCenterY = container.offsetHeight / 2;
+
+    // Number of categories
+    const numCategories = Object.keys(categorySizes).length;
+
+    // Angle between each category
+    const angleBetween = 360 / numCategories;
+
+    let currentAngle = 0;  // Start at 0 degrees
+
     for (let category in categorySizes) {
         const element = document.querySelector(`#${category}`);
         if (element) {
+            const radius = (container.offsetWidth - categorySizes[category]) / 2;
+
+            // Use trigonometry to position each category
+            const x = containerCenterX + radius * Math.cos(currentAngle * (Math.PI / 180)) - categorySizes[category] / 2;
+            const y = containerCenterY + radius * Math.sin(currentAngle * (Math.PI / 180)) - categorySizes[category] / 2;
+
+            element.style.left = `${x}px`;
+            element.style.top = `${y}px`;
+
             element.style.width = `${categorySizes[category]}px`;
             element.style.height = `${categorySizes[category]}px`;
+
+            currentAngle += angleBetween;
         }
     }
 });
